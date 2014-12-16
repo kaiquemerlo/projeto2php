c0baa2a8e4664fd844e3bcaa0069e1301271d439
Kaique Merlo <kaique.merlo@hotmail.com>
2014-12-16 20:34:15 -0200

    Iniciando

---
+++ index.php
@@ -0,0 +1,105 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>SURF</title>
+    <link href="css/style.css" rel="stylesheet" type="text/css"/>
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>    
+    <script type="text/javascript" src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+    
+</head>
+
+<body>
+
+<section>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-12 bg">
+                
+                <header>
+            
+                <div class="menu">
+                    <div class="container">
+
+                   
+                    <nav class="navbar navbar-inverse barra-style" id="barra-style">
+                     <div class="col-sm-5">
+                        <div class="navbar-header">
+                            <button class="navbar-toggle" data-target=".navbar-collapse" data-toggle="collapse" type="button">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                            <a class="navbar-brand" href="#">
+                                MARÉSURF
+                            </a>
+                        </div>
+                    </div><!--col-md-5-->
+                    
+                    <div class="col-sm-7">
+                    <div class="collapse navbar-collapse navbar-right">
+                        <ul class="nav navbar-nav">
+                            <li>
+                                <a href="#">Home</a>
+                            </li>
+                            <li>
+                                <a href="#">Empresa</a>
+                            </li>
+                            <li>
+                                <a href="#">Serviços</a>
+                            </li>
+                            <li>
+                                <a href="#">Contato</a>
+                            </li>
+                        </ul>
+                    </div><!-- container da navbar header-->
+                    </div><!-- navbar right-->
+
+                    </nav>
+
+
+                    <div class="col-xs-12 col-sm-12 col-md-12" id="box">
+                        <div class="col-xs-12 col-sm-12 col-md-12" >
+                        <h2>Aqui você encontra as melhores marcas:</h2>
+                        <div class="col-xs-12 col-sm-6 col-md-2">
+                            <img src="imagens/Quiksilver.png" width="190">
+                        </div>
+
+                        <div class="col-xs-12 col-sm-6 col-md-2 col-md-offset-1">
+                            <img src="imagens/rip-curl-logo.png" width="190">
+                        </div>
+
+                        <div class="col-xs-12 col-sm-6 col-md-2 col-md-offset-1">
+                            <img src="imagens/billabong-logo-2.jpg" width="190">
+                        </div>
+
+                        <div class="col-xs-12 col-sm-6 col-md-2 col-md-offset-1">
+                            <img src="imagens/hurley.png" width="190">
+                        </div>
+                          
+                        
+                        </div>
+                    </div>  
+
+                </header>
+
+                    
+
+                </div><!--menu-->
+
+            </div>
+        </div>
+    </div>
+
+    
+
+
+</section>
+
+</div>
+
+</body>
+</html>
---
+++ serviços.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kaiquemerlo
+ * Date: 04/12/2014
+ * Time: 20:19
+ */ 
