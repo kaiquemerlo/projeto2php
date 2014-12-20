37957a7d42888dccfdfe6550c9d90714566635f6
Kaique Merlo <kaique.merlo@hotmail.com>
2014-12-20 09:49:06 -0200

    ARRUMANDO A INDEX E CHAMADAS DE PAGINAS

--- index.php
+++ index.php
@@ -6,22 +6,26 @@
 
 $pg = $_GET['pg'];
 
+ if( $pg == ''){
+    require_once('home.php');
+ }
 
-if( ($pg != 'home.php') && ($pg != 'empresa.php') && ($pg != 'servicos.php') && ($pg != 'produtos.php') && ($pg != 'contato.php') ){
+
+if( ($pg != '') && ($pg != 'home.php') && ($pg != 'empresa.php') && ($pg != 'servicos.php') && ($pg != 'produtos.php') && ($pg != 'contato.php') ){ // se o valor de GET for dif desse mostra a pagina de erro.
     
      require_once('erro.php'); 
 
+ }elseif(!isset($pg)){ // se n exisitir o GET mostra a home
+ require_once('home.php');
+ }else{ // se n for nenhum dos parametros acima mostra a pagina requisitada em GET
+ 	require_once($_GET['pg']); 
  }
 
- if( $pg == ''){
-    require_once('home.php');
- }else{
- require_once($_GET['pg']); 
- }      
 
 
+      
 
-echo $pg;
+
 ?> 
 
                     
