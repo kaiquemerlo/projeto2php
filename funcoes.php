abf9413a43063ce70376e9ed9e0e386265e1404c
Kaique Merlo <kaique.merlo@hotmail.com>
2015-01-17 17:41:31 -0200

    Adicionando condicional para verf. se arquivo existe

--- funcoes.php
+++ funcoes.php
@@ -20,14 +20,16 @@ endif;
 
 $retirando = str_replace("/", "", $path); // trocando / por " " np path para verificar se o arquivo existe
 
-if(!in_array($retirando, $paginas)): // se a rota armazenada em $retirando n existir no array $paginas mostra a pagina de erros
+if(!in_array($retirando, $paginas) && !file_exists($retirando.".php")): // se a rota armazenada em $retirando n existir no array $paginas mostra a pagina de erros
 	header("HTTP/1.0 404 Not Found"); 
 header("Status: 404 Not Found");
 	require_once('erro.php');
 	require_once('inc/footer.php');
 	die;
 else:
+	if(file_exists($retirando.".php")):
 	require_once($path.".php"); // se existir mostre o arquivo
+	endif;
 endif;
 
 }
