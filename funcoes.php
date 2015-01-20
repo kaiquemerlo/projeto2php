563ca983257f1e5b76450ee99417d695dc9bd968
Kaique Merlo <kaique.merlo@hotmail.com>
2015-01-19 22:17:12 -0200

    Ajustando o arquivo de funcoes de acordo com orientacoes do tutor :)

--- funcoes.php
+++ funcoes.php
@@ -11,26 +11,23 @@ $paginas = array( // array guarda as rotas existentes
 );
 
 // verificando se o path esta na pagina raiz /
-if($path == "/"):
+if($path == "/"){
 	require_once('home.php'); // se estiver tras a pagina home
 	require_once('inc/footer.php');
 	die;
-endif;
+}
 
 
 $retirando = str_replace("/", "", $path); // trocando / por " " np path para verificar se o arquivo existe
 
-if(!in_array($retirando, $paginas) && !file_exists($retirando.".php")): // se a rota armazenada em $retirando n existir no array $paginas mostra a pagina de erros
+if(in_array($retirando, $paginas) && file_exists($retirando.".php")){ // se a rota armazenada em $retirando n existir no array $paginas mostra a pagina de erros
+	require_once($path.".php"); // se existir mostre o arquivo
+}else{
 	header("HTTP/1.0 404 Not Found"); 
-header("Status: 404 Not Found");
 	require_once('erro.php');
 	require_once('inc/footer.php');
 	die;
-else:
-	if(file_exists($retirando.".php")):
-	require_once($path.".php"); // se existir mostre o arquivo
-	endif;
-endif;
+}
 
 }
 
